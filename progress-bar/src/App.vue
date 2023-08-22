fe28894ddb0542dd2319caa6aef956a13a8db1ff
Leejaeyeop <dlwoduq78@naver.com>
2023-08-22 20:22:41 +0900

    리파지토리 최초 커밋
    -  progress bar 컴포넌트 최초 커밋

---
+++ progress-bar/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <ProgressBar />
+</template>
+
+<script>
+import ProgressBar from "./components/ProgressBar.vue";
+
+export default {
+    name: "App",
+    components: {
+        ProgressBar,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
